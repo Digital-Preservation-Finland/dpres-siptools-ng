df6d9d19ec8ba58b751d8ff0a9437e112efe02f0
Matias Wargelin <matias.wargelin@csc.fi>
2023-03-06 15:18:53 +0000

    TPASPKT-1047 Pack SIP to a tar file

--- siptools_ng/sip.py
+++ siptools_ng/sip.py
@@ -1,5 +1,5 @@
 """Module for Submission Information Package (SIP) handling."""
-import shutil
+import tarfile
 from datetime import datetime
 from pathlib import Path
 from typing import Union
@@ -39,7 +39,10 @@ class SIP:
             raise ValueError("SIP does not contain any digital objects.")
 
         output_filepath = Path(output_filepath)
-        output_filepath.mkdir()
+        if output_filepath.exists():
+            raise FileExistsError(
+                f"Given output filepath '{output_filepath}' exists already."
+            )
 
         if tmp_filepath is None:
             current_time = datetime.now().isoformat(timespec="seconds")
@@ -53,17 +56,9 @@ class SIP:
             tmp_filepath, str(sign_key_filepath)
         )
 
-        # Copy temporary files to final location
-        shutil.copy(
-            str(mets_tmp_filepath),
-            str(output_filepath / METS_FILENAME)
+        self._pack_files_to_tar(
+            output_filepath, mets_tmp_filepath, signature_tmp_filepath
         )
-        shutil.copy(
-            str(signature_tmp_filepath),
-            str(output_filepath / SIGNATURE_FILENAME)
-        )
-
-        self._copy_digital_objects_to_sip(output_filepath)
 
     def _write_mets(self, output_directory: Path) -> Path:
         """Write the METS document to given directory.
@@ -76,22 +71,6 @@ class SIP:
         mets_filepath = output_directory / METS_FILENAME
         self.mets.write(mets_filepath)
         return mets_filepath
-
-    def _copy_digital_objects_to_sip(self, output_filepath: Path) -> None:
-        """Copy the digital objects to their target location in the SIP.
-
-        :param output_filepath: Filepath where the SIP is created to.
-        """
-        for digital_object in self.mets.digital_objects:
-            digital_object_parent_dir = (
-                output_filepath / Path(digital_object.sip_filepath).parent
-            )
-            digital_object_parent_dir.mkdir(parents=True, exist_ok=True)
-
-            shutil.copy(
-               str(digital_object.source_filepath),
-               str(output_filepath / digital_object.sip_filepath)
-            )
 
     def _write_signature(
         self,
@@ -117,3 +96,28 @@ class SIP:
         signature_filepath = output_directory / SIGNATURE_FILENAME
         signature_filepath.write_bytes(signature)
         return signature_filepath
+
+    def _pack_files_to_tar(
+        self,
+        output_filepath: Path,
+        mets_filepath: Path,
+        signature_filepath: Path
+    ) -> None:
+        """Pack contents of the SIP to a tar file.
+
+        Packs the METS document, signature file and digital objects of the SIP.
+
+        :param output_filepath: Filepath where the packed SIP is written to.
+        :param mets_filepath: Filepath to the METS document (written in
+            advance) that is included in this SIP.
+        :param signature_filepath: Filepath to the signature file (written in
+            advance) that is included in this SIP.
+        """
+        with tarfile.open(output_filepath, "w") as tarred_sip:
+            tarred_sip.add(name=mets_filepath, arcname=METS_FILENAME)
+            tarred_sip.add(name=signature_filepath, arcname=SIGNATURE_FILENAME)
+            for digital_object in self.mets.digital_objects:
+                tarred_sip.add(
+                    name=digital_object.source_filepath,
+                    arcname=digital_object.sip_filepath
+                )
--- tests/test_sip.py
+++ tests/test_sip.py
@@ -1,11 +1,18 @@
 """Test SIPs."""
 import hashlib
+import tarfile
 
 import pytest
 from mets_builder import METS, MetsProfile, StructuralMap, StructuralMapDiv
 
 from siptools_ng.sip import SIP
 from siptools_ng.sip_digital_object import SIPDigitalObject
+
+
+def _extract_sip(sip_filepath, extract_filepath):
+    """Extract tarred SIP to given path."""
+    with tarfile.open(sip_filepath) as sip:
+        sip.extractall(extract_filepath)
 
 
 def test_creating_sip_with_zero_files():
@@ -29,11 +36,14 @@ def test_creating_sip_to_existing_filepath(simple_sip):
     """Test that trying to create a SIP to an existing filepath raises
     error.
     """
-    with pytest.raises(FileExistsError):
+    with pytest.raises(FileExistsError) as error:
         simple_sip.finalize(
             output_filepath="tests/data/test_file.txt",
             sign_key_filepath="tests/data/rsa-keys.crt"
         )
+    assert str(error.value) == (
+        "Given output filepath 'tests/data/test_file.txt' exists already."
+    )
 
 
 def test_mets_in_sip(tmp_path, simple_sip):
@@ -46,7 +56,10 @@ def test_mets_in_sip(tmp_path, simple_sip):
         tmp_filepath=tmp_filepath
     )
 
-    mets_filepath = output_filepath / "mets.xml"
+    extract_filepath = tmp_path / "test_mets_in_sip_extracted"
+    _extract_sip(output_filepath, extract_filepath)
+
+    mets_filepath = extract_filepath / "mets.xml"
     assert mets_filepath.exists()
 
     mets_contents = mets_filepath.read_text()
@@ -85,11 +98,14 @@ def test_file_location_in_sip(tmp_path):
         tmp_filepath=tmp_filepath
     )
 
-    assert output_filepath.is_dir()
-    assert (output_filepath / "data").is_dir()
-    assert (output_filepath / "data" / "files").is_dir()
-    assert (output_filepath / "data" / "files" / "test_file_1.txt").is_file()
-    assert (output_filepath / "data" / "files" / "test_file_2.txt").is_file()
+    extract_filepath = tmp_path / "test_file_location_in_sip_extracted"
+    _extract_sip(output_filepath, extract_filepath)
+
+    assert extract_filepath.is_dir()
+    assert (extract_filepath / "data").is_dir()
+    assert (extract_filepath / "data" / "files").is_dir()
+    assert (extract_filepath / "data" / "files" / "test_file_1.txt").is_file()
+    assert (extract_filepath / "data" / "files" / "test_file_2.txt").is_file()
 
 
 def test_signature_in_sip(tmp_path, simple_sip):
@@ -104,10 +120,13 @@ def test_signature_in_sip(tmp_path, simple_sip):
         tmp_filepath=tmp_filepath
     )
 
-    signature_filepath = output_filepath / "signature.sig"
+    extract_filepath = tmp_path / "test_signature_in_sip_extracted"
+    _extract_sip(output_filepath, extract_filepath)
+
+    signature_filepath = extract_filepath / "signature.sig"
     assert signature_filepath.is_file()
 
-    mets_filepath = output_filepath / "mets.xml"
+    mets_filepath = extract_filepath / "mets.xml"
     sha_hash = hashlib.sha1(mets_filepath.read_bytes()).hexdigest()
     assert f"mets.xml:sha1:{sha_hash}" in signature_filepath.read_text("utf-8")
 
@@ -128,3 +147,15 @@ def test_tmp_filepath_for_sip_finalization(tmp_path, simple_sip):
     )
     assert (tmp_filepath / "mets.xml").is_file()
     assert (tmp_filepath / "signature.sig").is_file()
+
+
+def test_sip_is_tar_file(tmp_path, simple_sip):
+    """Test that the finalized SIP is a tar file."""
+    output_filepath = tmp_path / "test_sip_is_tar_file_output"
+    tmp_filepath = tmp_path / "test_sip_is_tar_file_tmp"
+    simple_sip.finalize(
+        output_filepath=output_filepath,
+        sign_key_filepath="tests/data/rsa-keys.crt",
+        tmp_filepath=tmp_filepath
+    )
+    assert tarfile.is_tarfile(output_filepath)
