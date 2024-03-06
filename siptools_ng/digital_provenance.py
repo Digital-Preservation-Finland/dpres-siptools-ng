e3dc1f0a917c473a481cc5c199117c95ffa91914
Matias Wargelin <matias.wargelin@csc.fi>
2024-03-06 11:18:16 +0000

    TPASPKT-1225 Add method for creating file-scraper agent
    Agent metadata representing file-scraper will be needed to be added as
    agent for certain events, so add a convenience method to create that
    easily.
    
    Hard coded premis id (which is separate from the METS metadata id) for
    the agent is added to help with deduplicating the metadata: a random
    generated premis id would lead to unidentical metadata instances each
    time the method is called. With a constant premis id separate calls to
    the method lead to identical instances, and the mets-builder serializer
    can handle the metadata deduplication automatically.

--- siptools_ng/digital_provenance.py
+++ siptools_ng/digital_provenance.py
@@ -1,4 +1,5 @@
 """Module for easy creation of digital provenance metadata."""
+from file_scraper import __version__ as file_scraper_version
 from mets_builder.metadata import DigitalProvenanceAgentMetadata
 
 from siptools_ng import __version__
@@ -12,4 +13,15 @@ def get_siptools_ng_agent() -> DigitalProvenanceAgentMetadata:
         agent_version=__version__,
         agent_identifier_type="local",
         agent_identifier=f"fi-dpres-dpres-siptools-ng-{__version__}"
+    )
+
+
+def get_file_scraper_agent() -> DigitalProvenanceAgentMetadata:
+    """Return agent metadata representing file-scraper."""
+    return DigitalProvenanceAgentMetadata(
+        agent_name="file-scraper",
+        agent_type="software",
+        agent_version=file_scraper_version,
+        agent_identifier_type="local",
+        agent_identifier=f"fi-dpres-file-scraper-{file_scraper_version}"
     )
--- tests/test_digital_provenance.py
+++ tests/test_digital_provenance.py
@@ -1,4 +1,6 @@
 """Test digital provenance module."""
+from file_scraper import __version__ as file_scraper_version
+
 from siptools_ng import __version__, digital_provenance
 
 
@@ -11,4 +13,16 @@ def test_get_siptools_ng_agent():
     assert agent.agent_identifier_type == "local"
     assert agent.agent_identifier == (
         f"fi-dpres-dpres-siptools-ng-{__version__}"
+    )
+
+
+def test_get_file_scraper_agent():
+    """Test that agent representing file-scraper has correct information."""
+    agent = digital_provenance.get_file_scraper_agent()
+    assert agent.agent_name == "file-scraper"
+    assert agent.agent_type.value == "software"
+    assert agent.agent_version == file_scraper_version
+    assert agent.agent_identifier_type == "local"
+    assert agent.agent_identifier == (
+        f"fi-dpres-file-scraper-{file_scraper_version}"
     )
